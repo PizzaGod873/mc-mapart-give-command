5f4040a3e44277e2a4c92ac214002d2c743d802c
Andrew <150487080+PizzaGod873@users.noreply.github.com>
2024-06-12 18:52:51 -0500

    added main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+'''
+ATTENTION:
+This script requires 2 things to be installed.
+1. Python (https://www.python.org/downloads/)
+2: pyautogui (https://pyautogui.readthedocs.io/en/latest/install.html)
+
+--- CREDITS AND VIDEO LOCATIONS ---
+GITHUB: (link is being added at the time of recording this video) - PizzaGod873
+YOUTUBE: (link is being added at the time of recording this video) - @AndrewPCs
+REDDIT: https://www.reddit.com/user/SuperPizza999/ (Multiple posts so profile is listed) - SuperPizza999
+'''
+
+
+
+# This is stuff for the script that helps it work. (NOT RECOMMENDED TO CONFIGURE)
+
+import pyautogui as pag
+import time as t
+
+# MODIFY HERE
+
+t.sleep(7.5) # Delay to get into Minecraft
+
+for mmid in range(144, 152 + 1): # Modify the 1st and 2nd numbers to set the ID range.
+
+# This is what actually does the stuff. (NOT RECOMMENDED TO CONFIGURE)
+
+    pag.typewrite('t')
+    pag.typewrite('/give @s minecraft:filled_map{map:'+f'{mmid}'+'}')
+    pag.press('enter')
